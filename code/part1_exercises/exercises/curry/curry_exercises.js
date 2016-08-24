71643f0bca0a19d3539b6541a2263dd9aacce682
Nick Balestra <nbalestra@opentable.com>
2016-08-24 20:23:16 +0100

    more curry

--- code/part1_exercises/exercises/curry/curry_exercises.js
+++ code/part1_exercises/exercises/curry/curry_exercises.js
@@ -25,9 +25,8 @@ var sentences = _.map(words);
 //==============
 // Refactor to remove all arguments by partially applying the functions
 
-var filterQs = function(xs) {
-  return filter(function(x){ return match(/q/i, x);  }, xs);
-};
+// var filterQs = filter(function(x){ return match(/q/i, x);  });
+var filterQs = filter(_.match(/q/i));
 
 
 // Exercise 3
